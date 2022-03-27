0a96e06b0e5654e79f3bc2235c8858a6ca0d0704
nguoilangchai <hoxuantrieu20022000@gmail.com>
2022-03-27 22:23:20 +0700

    upload code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bai tap Android Week3 Nhóm 15
+
+- Link video : https://drive.google.com/drive/u/1/folders/1gukDKfnVx7S-X1_MUGNN5IGTP-u0pCdS
+
+
+# BaiTapTuan3
---
+++ app/src/main/java/com/example/baitaptuan3/DataStore.kt
@@ -0,0 +1,8 @@
+package com.example.baitaptuan3
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class DataStore(val username:String, val password:String):Parcelable {
+}
